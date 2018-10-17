77a58f0841d4edaa263a6fa479524c59ef4a8b09
janlandwehr <Jan.Landwehr@gmail.com>
2018-10-17 20:41:07 +0200

    Functions to create a user specific Config

--- src/BrokerCrawler.py
+++ src/BrokerCrawler.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import configparser
+import os
 
 #abstract broker
 class Broker(ABC):
@@ -19,26 +20,57 @@ class Config():
 
     def __init__(self):
 
-    def createConfig(self):
+        super().__init__()
+
+    def createConfig(self, ):
         config = configparser.ConfigParser()
         with open('settings.ini','w') as config_file:
             config.write(config_file)
+        return config
 
     def readConfig(self):
-        f = open('settings.ini')
-        config = f.read()
-        f.close()
-        return config
+        settings = configparser.ConfigParser()
+        settings._interpolation = configparser.ExtendedInterpolation()
+        settings.read('settings.ini')
+        return settings
 
     def getConfig(self):
         config = configparser.ConfigParser()
         readConfig = config.read('settings.ini')
         return readConfig
 
-    def creatSection(config, section):
-        config.add_section(section)
+    def creatSection(self, section):
+        config = configparser.ConfigParser()
+        config.read("settings.ini")
+        if not config.has_section(section):
+            config.add_section(section)
+        with open('settings.ini', 'w') as configfile:
+            config.write(configfile)
         return config
 
-    def addToSection(config, section, option, value):
+    def addToSection(self, section, option, value):
+        config = configparser.ConfigParser()
+        config.read("settings.ini")
         config.set(section,option,value)
+        with open('settings.ini', 'w') as configfile:
+            config.write(configfile)
         return config
+    def createNewConfig(self):
+
+        print("Creating Config")
+        if not os.path.exists('settings.ini'):
+            config = self.createConfig()
+        print("adding Binance broker")
+        self.creatSection("Binance")
+        pubKey=input("Enter public Key: ")
+        privKey=input("Enter priv Key: ")
+        self.addToSection("Binance","pubKey",pubKey)
+        self.addToSection( "Binance", "prvKey", privKey)
+        print ("config Created")
+        print("PubKey is: "+ self.readConfig().get("Binance","pubKey"))
+        return config
+
+config = Config()
+#config.createConfig()
+#inConfig = config.readConfig()
+iniConfig = config.createNewConfig()
